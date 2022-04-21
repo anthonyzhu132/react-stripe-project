fa4a8c6a6d782141d580581433ddb5b1394261ba
Anthony Zhu <anthonyzhu132@gmail.com>
2022-04-20 21:20:09 -0700

    added tslint config as well as initiating stripe

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv"
+import Stripe from "stripe";
+
+if (process.env.NODE_ENV !== 'production') {
+    config();
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET, {
+    apiVersion: '2020-08-27',
+})
