230505d35426f7de64aa66974bf161d284dc09f6
Anthony Zhu <anthonyzhu132@gmail.com>
2022-05-06 18:00:32 -0700

    adjusted api endpoint to accept /checkouts

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,28 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { createStripeCheckoutSession } from './checkout';
 
 export const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: true }));
 
+function runAsync(callback: Function) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    callback(req, res, next).catch(next);
+  };
+}
+
 app.post('/test', (req: Request, res: Response) => {
   const { amount } = req.body;
 
   res.status(200).send({ with_tax: amount * 7 });
 });
+
+app.post(
+  '/checkouts/',
+  runAsync(async ({ body }: Request, res: Response) => {
+    res.send(await createStripeCheckoutSession(body.line_items));
+  })
+);
--- src/checkout.ts
+++ src/checkout.ts
@@ -7,7 +7,7 @@ import { stripe } from './index';
 export async function createStripeCheckoutSession(
   line_items: Stripe.Checkout.SessionCreateParams.LineItem[],
 ) {
-  const url = 'http://localhost:3000'; // process.env.WEBAPP_URL;
+  const url = process.env.WEBAPP_URL;
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
