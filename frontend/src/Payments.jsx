9bb98a7307e1df7350dbcdbd9536c50ff722d0e0
Anthony Zhu <anthonyzhu132@gmail.com>
2022-05-09 19:16:45 -0700

    added jsx to render out a form to submit payment

--- frontend/src/Payments.jsx
+++ frontend/src/Payments.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import {
+  CardElement, useStripe, useElements, CardElement,
+} from '@stripe/react-stripe-js';
 import fetchFromApPI from './helpers';
 
 export default function Payments() {
@@ -18,6 +20,56 @@ export default function Payments() {
   };
 
   const handleSubmit = async (event) => {
-    
-  }
+    event.preventDefault();
+
+    const cardElement = elements.getElement(CardElement);
+
+    // Confirm Card Payment Details
+
+    const {
+      paymentIntent: updatedPaymentIntent,
+      error,
+    } = await stripe.confirmCardPayment(paymentIntent.client_secret, {
+      payment_method: { card: cardElement },
+    });
+
+    if (error) {
+      console.error(error);
+      error.payment_intent && setPaymentIntent(error.payment_intent);
+    } else {
+      setPaymentIntent(updatedPaymentIntent);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          type="number"
+          value={amount}
+          disabled={paymentIntent}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button
+          disabled={amount <= 0}
+          onClick={createPaymentIntent}
+          hidden={paymentIntent}
+          type="button"
+        >
+          `Ready to Pay $
+          {(amount / 100).toFixed(2)}
+          `
+        </button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+
+        <CardElement />
+        <button type="submit">
+          Pay
+        </button>
+      </form>
+
+    </>
+  );
 }
